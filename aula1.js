6de44570dc6a35c68d5c5a0cb0be014859a333aa
Allan <allannscsilva@gmail.com>
2021-11-25 12:27:31 -0300

    ok

--- aula1.js
+++ aula1.js
@@ -1,4 +1,4 @@
 var n1 = Number(window.prompt('digite um numero '))
 var n2 = Number(window.prompt('digite outro numero'))
 var s = n1 + n2
-window.alert(' soma dos numeros ' + s)
+window.alert(`soma dos numeros , ${n1} + ${n2} = ${s}`)
